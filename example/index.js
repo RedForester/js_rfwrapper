b94be3ab9cbc76b5f1a61bbec0d4faf12c0220ed
Deissh <hatollint@gmail.com>
2018-09-21 15:35:30 +0300

    переписал longpolling

--- example/index.js
+++ example/index.js
@@ -10,10 +10,6 @@ rf.event('node_created', (ctx) => {
     console.log(ctx)
 })
 
-rf.on((ctx) => {
-    console.log(ctx)
-})
-
 rf.initPolling('1bcf5ff3-c392-43db-8e6f-e8090210b0f7').catch((err) => {
     console.error(err)
 })
