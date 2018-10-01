8a6ef5c78a47f0e820ad8777ea8cb5adf4e95e0c
deissh <kudryavtsev@nppsatek.ru>
2018-10-01 17:50:10 +0300

    api.global добавил search

--- src/api/global.js
+++ src/api/global.js
@@ -9,3 +9,17 @@ export async function sendBatch (body) {
     const res = await axios.post(`/api/batch`, body, this.settings.axios)
     return res.data
 }
+
+/**
+ * Поиск узлов по картам
+ * @param {string} query
+ * @param {list} maps
+ * @return {Promise<>}
+ */
+export async function search(query, maps) {
+    const res = await axios.post(`/api/batch`, {
+        query,
+        map_ids: maps
+    }, this.settings.axios)
+    return res.data
+}
