feabbb6ac932efd48bf72f9b103051d8a595ae90
deissh <kudryavtsev@nppsatek.ru>
2018-10-10 15:59:27 +0300

    добавил тесты создания для класса узла

--- test/node.js
+++ test/node.js
@@ -29,4 +29,31 @@ describe('Node class', () => {
             }
         })
     })
+
+    describe('#json', () => {
+        it('should be valide json', async () => {
+            const node = await rf.node('adasd').json()
+            node.should.to.be.a('object')
+        })
+    })
+
+    describe('#create', () => {
+        it('should be valide params', () => {
+            try {
+                rf.node.create()
+            } catch (err) {
+                it.should.throw(Error)
+            }
+        })
+
+        it('should create new node as children in root node', async () => {
+            await rf.node.create('some title')
+        })
+
+        it('should create new node as children in node', async () => {
+            await rf.node.create('some title', {
+                parrent: '1efdsayf8asdfsewsdr92cm3'
+            })
+        })
+    })
 })
