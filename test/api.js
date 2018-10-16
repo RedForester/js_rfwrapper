18532c69b246c864e673fbd08519d4d203c03b5c
deissh <kudryavtsev@nppsatek.ru>
2018-10-16 16:08:55 +0300

    добавил базовые тесты для api.global и api.user

--- test/api.js
+++ test/api.js
@@ -12,18 +12,86 @@ const rf = new rfwrapper({
 
 describe('Api methods', () => {
     // todo: добавил тесты на проверку содержимого
+    describe('#user', () => {
+        const user = rf.api.user
+
+        it('@get: it should return current user', async () => {
+            const data = await user.get()
+            data.should.to.be.a('object')
+        })
+
+        it('@get: it should return error by unknown user', async () => {
+            try {
+                await user.get('1')
+            } catch (err) {
+                it.should.throw(Error)
+            }
+        })
+    })
+
     describe('#global', () => {
         const global = rf.api.global
 
-        it('getSID: it should to be a number', async () => {
+        it('@getSID: it should to be a number', async () => {
             const data = await global.getSID()
             data.should.to.be.a('number')
         })
 
-        it('getMaps: it should to be a array of objects', async () => {
+        it('@getKV: it should to be a object', async () => {
+            const data = await global.getKV()
+            data.should.to.be.a('object')
+        })
+
+        it('@exceptions: it should to be a array', async () => {
+            const data = await global.exceptions()
+            data.should.to.be.a('object')
+        })
+
+        it('@getMaps: it should to be a array of objects', async () => {
             const data = await global.getMaps()
             data.should.to.be.a('array')
             data.forEach(item => item.should.to.be.a('object'))
+        })
+
+        describe('@sendBatch', () => {
+            it('it should send Get request', async () => {
+                const data = await global.sendBatch([
+                    {
+                        url: '/api/maps',
+                        method: 'GET'
+                    }
+                ])
+                data.should.to.be.a('array')
+                data.forEach(item => {
+                    item.should.to.be.a('object')
+                })
+            })
+
+            it('it should send Post and Delete request', async () => {
+                // тестируем оба метода сразу что бы не мусорить после себя
+                let data = await global.sendBatch([
+                    {
+                        url: '/api/maps',
+                        method: 'POST',
+                        body: JSON.stringify({
+                            name: '123'
+                        })
+                    }
+                ])
+                data.should.to.be.a('array')
+                data.forEach(item => {
+                    item.should.to.be.a('object')
+                })
+
+                const node = JSON.parse(data[0].body)
+
+                await global.sendBatch([
+                    {
+                        url: '/api/maps/' + node.id,
+                        method: 'DELETE'
+                    }
+                ])
+            })
         })
     })
 })
